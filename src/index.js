a34306743034300b97e8562e329dabe89333af47
DiegoRojas <df.rojas.marcial@gmail.com>
2024-02-01 19:50:27 -0600

    update

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import app from "./app.js";
 import {connectDB} from './db.js'
 
 connectDB();
+/*
 app.listen(4000)
 console.log('Server on port',4000)
+*/
+
+// Servidor HTTP
+app.listen(process.env.HTTP_PORT, process.env.IP || 4000);
+console.log(process.env.IP)
